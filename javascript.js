7a1cb4f1ac7c34b3626fb0556fb506e4bd1e176f
Joshua Foate <joshuafoate@gmail.com>
2024-01-14 20:06:32 -0600

    Created functions for math operations (add, subtract, multiply, divide), variables for the operation, and an operate function.

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// Math operation functions
+
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract(a, b) {
+    let sum = a - b;
+    return sum;
+}
+
+function multiply(a, b) {
+    let product = a * b;
+    return product;
+}
+
+function divide(a, b) {
+    let product = a / b;
+    return product;
+}
+
+//////////////////////////
+
+// Variables for operation
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+//////////////////////////
+
+// Operate Function
+
+function operate(number1, number2, op) {
+    add();
+    subtract();
+    multiply();
+    divide();
+}
+
+//////////////////////////
